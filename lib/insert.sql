2faccf05c8db920117283cc457b0bd5fb7e4752f
Allie Soldau <alliesoldau@gmail.com>
2022-12-14 13:22:59 -0500

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 27, "M", "pink", "kind", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 13, "F", "orange", "cheerful", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 3, "F", "black", "playful", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 1, "M", "white", "angry", 0);
+INSERT INTO bears VALUES (5,"Melissa", 92, "F", "grey", "sleepy", 0);
+INSERT INTO bears VALUES (6,"Grinch", 16, "M", "green", "spiteful", 1);
+INSERT INTO bears VALUES (7,"Wendy", 38, "M", "blue", "wild", 1);
+INSERT INTO bears VALUES (8,NULL, 51, "F", "red", "perfect", 1);
+
